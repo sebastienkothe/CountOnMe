ef05ec7f310c7da8945343c9b27d477326905f9d
Sébastien Kothé <sebastien.kothe@icloud.com>
2020-06-04 12:15:06 +0200

    Added isolateNonPriorityOperations()

--- CountOnMe/Model/Calculator.swift
+++ CountOnMe/Model/Calculator.swift
@@ -69,20 +69,38 @@ class Calculator {
         textToCompute.append(mathOperator.symbol)
     }
     
+    fileprivate func isolateNonPriorityOperations(_ remainingFromCalculation: inout [String], _ operationsToReduce: inout [String], operatorIndex: Int, numberIndex: Int) {
+        remainingFromCalculation.append(contentsOf: [operationsToReduce[operatorIndex], operationsToReduce[numberIndex]])
+    }
+    
     func resolveOperation() throws {
+        guard lastElementIsNumber && hasEnoughElements && !hasAResult && !worthZero else {
+            throw CalculatorError.cannotAddEqualSign }
+        
         // Create local copy of operations
         var operationsToReduce = elements
-        
-        guard lastElementIsNumber && hasEnoughElements && !hasAResult && !worthZero else {
-            throw CalculatorError.cannotAddEqualSign
-        }
-        
+        var remainingFromCalculation: [String] = []
+        var result = 0.0
         // Iterate over operations while an operand still here
         while operationsToReduce.count > 1 {
+            var operatorRecovered = operationsToReduce[1]
+            
+            while (operatorRecovered == "+" || operatorRecovered == "-") && (operationsToReduce.contains("*") || operationsToReduce.contains("/")) {
+                
+                if result > 0 {
+                    isolateNonPriorityOperations(&remainingFromCalculation, &operationsToReduce, operatorIndex: 1, numberIndex: 2)
+                    guard let resultToSave = operationsToReduce.first else { return }
+                    operationsToReduce = Array(operationsToReduce.dropFirst(3))
+                    operationsToReduce.insert(resultToSave, at: 0)
+                } else {
+                    isolateNonPriorityOperations(&remainingFromCalculation, &operationsToReduce, operatorIndex: 1, numberIndex: 0)
+                    operationsToReduce = Array(operationsToReduce.dropFirst(2))
+                }
+                operatorRecovered = operationsToReduce[1]
+                result = 0.0
+            }
             
             guard let operandLeft = Double(operationsToReduce[0]), let operandRight = Double(operationsToReduce[2]) else { textToCompute = errorMessage ; return }
-            let operatorRecovered = operationsToReduce[1]
-            var result = 0.0
             
             switch operatorRecovered {
             case "+": result = operandLeft + operandRight
@@ -96,6 +114,7 @@ class Calculator {
             operationsToReduce = Array(operationsToReduce.dropFirst(3))
             operationsToReduce.insert("\(result)", at: 0)
         }
+        
         textToCompute.append(" = \(operationsToReduce.first!)")
     }
 }
